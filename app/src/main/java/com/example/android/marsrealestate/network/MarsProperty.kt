0b94913c8d65dcfba020e4182950237821d7299e
lakshmi-p15 <lakshmi.pavuluri15@gmail.com>
2022-09-10 12:22:40 -0700

    Added Moshi Library to get parsed Json data

--- app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
@@ -17,4 +17,11 @@
 
 package com.example.android.marsrealestate.network
 
-class MarsProperty()
+import com.squareup.moshi.Json
+
+data class MarsProperty(
+    val id: String,
+    @Json(name = "img_src")val imageSrcUrl: String,
+    val type: String,
+    val price: Double
+)
